3df463488e2e57db779aed57d81068c970c554d2
Albert Pratomo <albertpratomo21@gmail.com>
2023-03-23 00:04:07 +0100

    chore: install storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
 
-// https://astro.build/config
-export default defineConfig({});
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
+export default defineConfig({
+    integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                // Add your components here
+            },
+            apiOptions: {
+                region: 'eu',
+            },
+        })
+    ],
+});
