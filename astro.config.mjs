08378729d67f66af4d4a7d28c359d1ce70b0fde4
Albert Pratomo <albertpratomo21@gmail.com>
2023-03-27 23:53:30 +0200

    chore: install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
+import tailwind from "@astrojs/tailwind";
 
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 export default defineConfig({
-    integrations: [
-        storyblok({
-            accessToken: env.STORYBLOK_TOKEN,
-            components: {
-                // Add your components here
-            },
-            apiOptions: {
-                region: 'eu',
-            },
-        })
-    ],
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+      },
+      apiOptions: {
+        region: 'eu'
+      }
+    }), 
+    tailwind(),
+  ],
 });
