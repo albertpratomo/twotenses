0f2a2bcdecaeee3e4ece3c3d2e4089656a1b52b5
Albert Pratomo <albertpratomo21@gmail.com>
2023-03-30 03:49:56 +0200

    feat: configure storyblok integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 import {defineConfig} from 'astro/config';
-// import storyblok from '@storyblok/astro';
-// import { loadEnv } from 'vite';
+import storyblok from '@storyblok/astro';
+import {loadEnv} from 'vite';
 import tailwind from '@astrojs/tailwind';
 
-// const env = loadEnv('', process.cwd(), 'STORYBLOK');
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
 export default defineConfig({
     integrations: [
-        // storyblok({
-        //     accessToken: env.STORYBLOK_TOKEN,
-        //     components: {
-        //         // Add your components here
-        //     },
-        //     apiOptions: {
-        //         region: 'eu',
-        //     },
-        // }),
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                Headline: 'storyblok/Headline',
+                Hero: 'storyblok/Hero',
+                Page: 'storyblok/Page',
+            },
+        }),
         tailwind(),
     ],
 });
