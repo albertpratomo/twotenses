e4c1976b3a67a2b895d36cf0d2bfba341461f655
Albert Pratomo <albertpratomo21@gmail.com>
2023-03-30 21:47:09 +0200

    feat: create counter vue component

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+
+const props = defineProps({
+    from: {
+        default: 0,
+        type: [Number, String],
+    },
+});
+
+const count = ref(parseInt(props.from));
+</script>
+
+<template>
+    <div class="flex gap-4 text-6xl text-white">
+        <button @click="count--">
+            -
+        </button>
+
+        <div>
+            {{ count }}
+        </div>
+
+        <button @click="count++">
+            +
+        </button>
+    </div>
+</template>
