5afce26a31d618ddf1e169cfb4b7eded1ed8cb1f
Albert Pratomo <albertpratomo21@gmail.com>
2023-04-22 21:53:28 +0200

    feat: add socials to hamburger menu

--- src/components/HamburgerMenu.vue
+++ src/components/HamburgerMenu.vue
@@ -3,13 +3,6 @@ import {ref} from 'vue';
 import {onClickOutside} from '@vueuse/core';
 import IHamburger from '@/components/icons/IHamburger.vue';
 
-defineProps({
-    links: {
-        required: true,
-        type: Array,
-    },
-});
-
 const isOpen = ref(false);
 
 const menu = ref(null);
@@ -28,17 +21,9 @@ onClickOutside(menu, () => isOpen.value = false);
         <Transition name="slide-left">
             <div
                 v-if="isOpen"
-                class="fixed inset-y-0 right-0 bg-black p-4 pt-16"
+                class="fixed inset-y-0 right-0 bg-black p-4 pt-20"
             >
-                <nav class="flex flex-col gap-4 text-right">
-                    <a
-                        v-for="(link, i) in links"
-                        :key="i"
-                        :href="link.url.cached_url"
-                    >
-                        {{ link.label }}
-                    </a>
-                </nav>
+                <slot />
             </div>
         </Transition>
     </div>
