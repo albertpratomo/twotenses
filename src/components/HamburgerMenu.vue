bdc8aa1796b5376d231d79a312c5ec4aa6aba2b1
Albert Pratomo <albertpratomo21@gmail.com>
2023-04-22 17:47:44 +0200

    feat: create hamburger menu icon

---
+++ src/components/HamburgerMenu.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+import IHamburger from '@/components/icons/IHamburger.vue';
+
+defineProps({
+    links: {
+        required: true,
+        type: Array,
+    },
+});
+
+const isOpen = ref(false);
+</script>
+
+<template>
+    <div>
+        <IHamburger
+            :is-open="isOpen"
+            @toggle="isOpen = !isOpen"
+        />
+    </div>
+</template>
---
+++ src/components/icons/IHamburger.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+    isOpen: {
+        default: false,
+        type: Boolean,
+    },
+});
+
+defineEmits(['toggle']);
+</script>
+
+<template>
+    <button
+        class="flex h-6 w-6 flex-col justify-between transition-transform"
+        :class="isOpen ? '-rotate-45' : ''"
+        @click="$emit('toggle')"
+    >
+        <div
+            class="h-1 w-1/2 origin-right bg-red transition-transform delay-150"
+            :class="isOpen ? '-rotate-90 -translate-y-0.5' : ''"
+        />
+
+        <div class="h-1 w-full bg-red" />
+
+        <div
+            class="h-1 w-1/2 origin-left self-end bg-red transition-transform delay-150"
+            :class="isOpen ? '-rotate-90 translate-y-0.5' : ''"
+        />
+    </button>
+</template>
