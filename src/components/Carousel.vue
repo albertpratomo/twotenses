cfed43debb21b666c833a1ad9b46c3d90b32bd3c
Albert Pratomo <albertpratomo21@gmail.com>
2023-04-24 00:17:02 +0200

    feat: make carousel functional

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import {useCycleList} from '@vueuse/core';
+import IArrow from '@/components/icons/IArrow.vue';
+
 const props = defineProps({
     items: {
         required: true,
@@ -6,49 +9,75 @@ const props = defineProps({
     },
 });
 
-const item = props.items[0];
+// TODO: animate slide left right
+// TODO: interval next every 2s
+// TODO: swipe left right
+// TODO: responsiveness check
+const {state: item, next, prev, index} = useCycleList(props.items);
 </script>
 
 <template>
-    <div class="relative">
-        <picture>
-            <source
-                media="(max-width: 639px)"
-                :srcset="`${item.image_portrait.url}639x852`"
-            >
-
-            <source
-                media="(max-width: 767px)"
-                :srcset="`${item.image_landscape.url}767x431`"
-            >
-
-            <source
-                media="(max-width: 1023px)"
-                :srcset="`${item.image_portrait.url}1023x1364`"
-            >
-
-            <img
-                :alt="item.image_landscape.alt"
-                :src="`${item.image_landscape.url}1920x1080`"
-                :title="item.image_landscape.title"
-            >
-        </picture>
-
-        <div
-            class="container-fluid absolute inset-x-0 bottom-8 flex justify-between text-2xl"
-            :class="item.text_color"
+    <div
+        class="relative"
+        :class="item.text_color"
+    >
+        <Transition
+            mode="out-in"
+            name="fade"
         >
-            <p>
-                {{ item.description }}
-            </p>
+            <div :key="index">
+                <picture>
+                    <source
+                        media="(max-width: 639px)"
+                        :srcset="`${item.image_portrait.url}639x852`"
+                    >
 
-            <div class="flex divide-x-2">
-                <span class="pr-4">01</span>
+                    <source
+                        media="(max-width: 767px)"
+                        :srcset="`${item.image_landscape.url}767x431`"
+                    >
 
-                <span class="pl-4">
-                    0{{ items.length }}
-                </span>
+                    <source
+                        media="(max-width: 1023px)"
+                        :srcset="`${item.image_portrait.url}1023x1364`"
+                    >
+
+                    <img
+                        :alt="item.image_landscape.alt"
+                        :src="`${item.image_landscape.url}1920x1080`"
+                        :title="item.image_landscape.title"
+                    >
+                </picture>
+
+                <div class="container-fluid absolute inset-x-0 bottom-8 flex items-center justify-between md:text-2xl">
+                    <p>
+                        {{ item.description }}
+                    </p>
+
+                    <div class="flex divide-x-2">
+                        <span class="pr-4">
+                            {{ `${index + 1}`.padStart(2, '0') }}
+                        </span>
+
+                        <span class="pl-4">
+                            {{ `${items.length}`.padStart(2, '0') }}
+                        </span>
+                    </div>
+                </div>
             </div>
+        </Transition>
+
+        <div class="inset-y-center container-fluid inset-x-0 flex justify-between">
+            <IArrow
+                class="cursor-pointer"
+                direction="left"
+                @click="prev()"
+            />
+
+            <IArrow
+                class="cursor-pointer"
+                @click="next()"
+            />
         </div>
     </div>
 </template>
---
+++ src/components/icons/IArrow.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+    direction: {
+        default: 'right',
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <svg
+        class="h-14"
+        :class="direction === 'left' ? 'rotate-180' : ''"
+        fill="none"
+        viewBox="0 0 56 51"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M.173 25.572l53.468.026M29.642 1.599l24.049 23.949L29.619 49.72"
+            stroke="currentColor"
+            stroke-width="2"
+        />
+    </svg>
+</template>
