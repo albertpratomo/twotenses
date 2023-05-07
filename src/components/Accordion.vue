10fd00270b8417a0c975f61c172fe21d60e466cd
Albert Pratomo <albertpratomo21@gmail.com>
2023-05-07 23:47:46 +0200

    feat: show image in accordion content

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -18,24 +18,65 @@ const isOpen = ref(props.items.map(_ => false));
         :key="i"
         class="container-fluid border-b border-white"
     >
-        <button
-            class="flex w-full items-center justify-between border-l border-white py-4 text-4xl"
-            @click="isOpen[i] = !isOpen[i]"
-        >
-            {{ item.title }}
-
-            <IPlus v-if="item.image.title !== null" />
-
-            <div
-                v-else
-                class="flex h-14 items-center text-white"
+        <div class="relative border-l border-white">
+            <button
+                class="flex w-full items-center justify-between py-4 pr-3 text-4xl"
+                @click="isOpen[i] = !isOpen[i]"
             >
-                {{ item.content }}
-            </div>
-        </button>
+                {{ item.title }}
 
-        <div v-if="isOpen[i] && item.image.title !== null">
-            {{ item.content }}
+                <IPlus
+                    v-if="item.image.title !== null"
+                    class="z-10 transition-transform duration-300 ease-in-out"
+                    :class="{ '-rotate-45': isOpen[i] }"
+                />
+
+                <div
+                    v-else
+                    class="flex h-14 items-center text-white"
+                >
+                    {{ item.content }}
+                </div>
+            </button>
+
+            <Transition name="fade">
+                <div
+                    v-if="isOpen[i] && item.image.title !== null"
+                    class="mt-28 max-w-md pb-6"
+                >
+                    {{ item.content }}
+                </div>
+            </Transition>
+
+            <Transition name="fade">
+                <picture
+                    v-if="isOpen[i] && item.image.title !== null"
+                    class="pointer-events-none absolute inset-y-0 right-0"
+                >
+                    <source
+                        media="(max-width: 639px)"
+                        :srcset="`${item.image.url}639x852`"
+                    >
+
+                    <source
+                        media="(max-width: 767px)"
+                        :srcset="`${item.image.url}767x431`"
+                    >
+
+                    <source
+                        media="(max-width: 1023px)"
+                        :srcset="`${item.image.url}1023x1364`"
+                    >
+
+                    <img
+                        :alt="item.image.alt"
+                        class="h-full"
+                        loading="lazy"
+                        :src="`${item.image.url}672x388`"
+                        :title="item.image.title"
+                    >
+                </picture>
+            </Transition>
         </div>
     </div>
 </template>
