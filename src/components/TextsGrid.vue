c8635b5384d076230ed807c8881fd4b4149da4a2
Albert Pratomo <albertpratomo21@gmail.com>
2023-05-10 00:06:50 +0200

    feat: animate TextsGrid

--- src/components/TextsGrid.vue
+++ src/components/TextsGrid.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import {useCycleList} from '@vueuse/core';
+
 const props = defineProps({
     items: {
         required: true,
@@ -6,16 +8,30 @@ const props = defineProps({
     },
 });
 
-const texts = props.items[0];
+const {state: texts, next} = useCycleList(props.items);
+
+setInterval(next, 4000);
 </script>
 
 <template>
-    <div class="min-h-screen max-w-4xl flex-col justify-center gap-8 border-x border-white text-8xl leading-[.67]">
-        <div
-            v-for="text, i in texts"
-            :key="i"
-            class="border-y border-white"
-            v-html="text"
-        />
+    <div class="container-fluid">
+        <div class="h-[100svh] max-w-4xl flex-col justify-center gap-8 border-x border-white text-8xl leading-[.67]">
+            <div
+                v-for="text, i in texts"
+                :key="i"
+                class="border-y border-white"
+            >
+                <Transition
+                    mode="out-in"
+                    name="fade-up"
+                >
+                    <div
+                        :key="text"
+                        :style="{ transitionDelay: `calc(${i} * 120ms)` }"
+                        v-html="text"
+                    />
+                </Transition>
+            </div>
+        </div>
     </div>
 </template>
