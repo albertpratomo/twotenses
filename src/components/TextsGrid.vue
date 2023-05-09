16dc3376c7c89ff6951f1a5ee5e972c542d83dac
Albert Pratomo <albertpratomo21@gmail.com>
2023-05-09 23:27:14 +0200

    feat: create base TextsGrid component

---
+++ src/components/TextsGrid.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+    items: {
+        required: true,
+        type: Array,
+    },
+});
+
+const texts = props.items[0];
+</script>
+
+<template>
+    <div class="min-h-screen max-w-4xl flex-col justify-center gap-8 border-x border-white text-8xl leading-[.67]">
+        <div
+            v-for="text, i in texts"
+            :key="i"
+            class="border-y border-white"
+            v-html="text"
+        />
+    </div>
+</template>
