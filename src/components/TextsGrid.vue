1e768e488546561f647a4ad3f4c9d3b97aad5ab5
Albert Pratomo <albertpratomo21@gmail.com>
2023-05-11 00:35:29 +0200

    feat: finalize borders of TextsGrid

--- src/components/TextsGrid.vue
+++ src/components/TextsGrid.vue
@@ -14,24 +14,42 @@ setInterval(next, 4000);
 </script>
 
 <template>
-    <div class="container-fluid">
-        <div class="h-[100svh] max-w-4xl flex-col justify-center gap-8 border-x border-white text-8xl leading-[.67]">
+    <div class="h-[100svh] flex-col">
+        <div class="container-fluid w-full grow">
+            <div class="h-full max-w-4xl border-x border-white" />
+        </div>
+
+        <template
+            v-for="text, i in texts"
+            :key="i"
+        >
             <div
-                v-for="text, i in texts"
-                :key="i"
-                class="border-y border-white"
+                v-if="i > 0"
+                class="container-fluid w-full"
             >
-                <Transition
-                    mode="out-in"
-                    name="fade-up"
-                >
-                    <div
-                        :key="text"
-                        :style="{ transitionDelay: `calc(${i} * 120ms)` }"
-                        v-html="text"
-                    />
-                </Transition>
+                <div class="h-9 max-w-4xl border-x border-white" />
             </div>
+
+            <div class="border-y border-white text-8xl leading-[.67]">
+                <div class="container-fluid">
+                    <div class="max-w-4xl border-x border-white">
+                        <Transition
+                            mode="out-in"
+                            name="fade-up"
+                        >
+                            <div
+                                :key="text"
+                                :style="{ transitionDelay: `calc(${i} * 120ms)` }"
+                                v-html="text"
+                            />
+                        </Transition>
+                    </div>
+                </div>
+            </div>
+        </template>
+
+        <div class="container-fluid w-full grow">
+            <div class="h-full max-w-4xl border-x border-white" />
         </div>
     </div>
 </template>
