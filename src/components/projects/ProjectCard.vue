815020a712c12f1b3d0f13ac979e1d0665e8a9a6
Albert Pratomo <albertpratomo21@gmail.com>
2023-05-14 00:03:30 +0200

    refactor: extract out ProjectCard component

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps({
+    project: {
+        required: true,
+        type: Object,
+    },
+});
+</script>
+
+<template>
+    <a
+        class="uppercase text-gray"
+        :href="project.href"
+    >
+        <picture>
+            <source
+                media="(max-width: 414px)"
+                :srcset="`${project.thumbnail.url}414x276`"
+            >
+
+            <img
+                :alt="project.thumbnail.alt"
+                height="320"
+                loading="lazy"
+                :src="`${project.thumbnail.url}570x380`"
+                :title="project.thumbnail.title"
+                width="570"
+            >
+        </picture>
+
+        <div class="mt-2">
+            {{ project.name }}
+        </div>
+    </a>
+</template>
---
+++ src/components/projects/ProjectsGrid.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import ProjectCard from '@/components/projects/ProjectCard.vue';
+
+defineProps({
+    categories: {
+        required: true,
+        type: Array,
+    },
+    industries: {
+        required: true,
+        type: Array,
+    },
+    projects: {
+        required: true,
+        type: Array,
+    },
+});
+</script>
+
+<template>
+    <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 sm:gap-x-4 md:gap-y-12 lg:grid-cols-3">
+        <ProjectCard
+            v-for="(project, i) in projects"
+            :key="i"
+            :project="project"
+        />
+    </div>
+
+    <div v-if="projects.length === 0">
+        No projects found for the active filter.
+    </div>
+</template>
