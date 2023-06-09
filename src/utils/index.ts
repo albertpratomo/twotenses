b76633e74cfb298f28d6b789d3198566f7065501
Albert Pratomo <albertpratomo21@gmail.com>
2023-06-10 00:54:37 +0200

    refactor: move formatLink to main utils file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,3 +12,21 @@ export function getSrcset(image: ISbImage, width: number, height = 0) {
         ${url}${width}x${height}
     `;
 }
+
+interface ISbLink {
+    cached_url: string
+    target?: string
+}
+
+export function formatLink(link: ISbLink) {
+    const href = link.cached_url.startsWith('http')
+        ? link.cached_url
+        : `/${link.cached_url === 'home' ? '' : link.cached_url}`;
+
+    const target = link.target || '_self';
+
+    return {
+        href,
+        target,
+    };
+}
