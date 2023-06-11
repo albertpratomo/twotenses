d7f81f5e366eef294d447c0de9a5903386871e57
Albert Pratomo <albertpratomo21@gmail.com>
2023-06-11 18:06:37 +0200

    fix: fix Accordion layout

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -23,14 +23,14 @@ const isOpen = ref(props.items.map(_ => false));
         <div class="container-fluid px-container">
             <div class="relative border-l border-white">
                 <button
-                    class="flex w-full items-center justify-between py-4 text-2xl md:text-4xl xl:pr-3"
+                    class="flex w-full items-center justify-between py-4 text-2xl md:text-4xl lg:pr-3"
                     @click="isOpen[i] = !isOpen[i]"
                 >
                     {{ item.title }}
 
                     <IPlus
                         v-if="item.image.title !== null"
-                        class="z-10 transition-transform duration-300 ease-in-out"
+                        class="transition-transform duration-300 ease-in-out"
                         :class="{ '-rotate-45': isOpen[i] }"
                         size="h-8 md:h-14"
                     />
@@ -44,49 +44,46 @@ const isOpen = ref(props.items.map(_ => false));
                 </button>
 
                 <Transition name="fade">
-                    <picture
-                        v-if="isOpen[i] && item.image.title !== null"
-                        class="pointer-events-none inset-y-0 right-0 xl:absolute"
-                    >
-                        <source
-                            height="359"
-                            media="(max-width: 639px)"
-                            :srcset="getSrcset(item.image, 639, 359)"
-                            width="639"
-                        >
-
-                        <source
-                            height="431"
-                            media="(max-width: 767px)"
-                            :srcset="getSrcset(item.image, 767, 431)"
-                            width="767"
-                        >
-
-                        <source
-                            height="575"
-                            media="(max-width: 1023px)"
-                            :srcset="getSrcset(item.image, 1023, 575)"
-                            width="1023"
-                        >
-
-                        <img
-                            :alt="item.image.alt"
-                            class="w-auto xl:h-full"
-                            height="388"
-                            loading="lazy"
-                            :srcset="getSrcset(item.image, 672, 388)"
-                            :title="item.image.title"
-                            width="672"
-                        >
-                    </picture>
-                </Transition>
-
-                <Transition name="fade">
                     <div
                         v-if="isOpen[i] && item.image.title !== null"
-                        class="mt-4 pb-4 md:pb-6 xl:mt-28 xl:max-w-md"
+                        class="pointer-events-none flex flex-col justify-between lg:mt-[-5.5rem] lg:flex-row"
                     >
-                        {{ item.content }}
+                        <picture class="sm:w-1/2">
+                            <source
+                                height="359"
+                                media="(max-width: 639px)"
+                                :srcset="getSrcset(item.image, 639, 359)"
+                                width="639"
+                            >
+
+                            <source
+                                height="431"
+                                media="(max-width: 767px)"
+                                :srcset="getSrcset(item.image, 767, 431)"
+                                width="767"
+                            >
+
+                            <source
+                                height="575"
+                                media="(max-width: 1023px)"
+                                :srcset="getSrcset(item.image, 1023, 575)"
+                                width="1023"
+                            >
+
+                            <img
+                                :alt="item.image.alt"
+                                class="w-full"
+                                height="388"
+                                loading="lazy"
+                                :srcset="getSrcset(item.image, 672, 388)"
+                                :title="item.image.title"
+                                width="672"
+                            >
+                        </picture>
+
+                        <div class="py-4 md:py-6 lg:order-first lg:mt-auto lg:w-1/2 lg:pr-16 lg:pt-20">
+                            {{ item.content }}
+                        </div>
                     </div>
                 </Transition>
             </div>
