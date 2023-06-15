4f4a4f9cfb9b8dbd55817dc780cd599115a0c206
Albert Pratomo <albertpratomo21@gmail.com>
2023-06-16 00:57:37 +0200

    feat: update onion koala quiz

--- src/components/OnionKoala.vue
+++ src/components/OnionKoala.vue
@@ -4,29 +4,119 @@ import {ref} from 'vue';
 
 const slides = [
     {
-        button: 'Yes... muah!',
+        buttons: ['Your lovely bb 🥰', 'Your cranky bb 🥺'],
         image: 'onion-koala.png',
         title: `
             Helloooo...? Helloooo...?<br>
-            Am I talking to my bb? 
+            Who's there? 👀
         `,
     },
     {
-        answer: ['12-01-23'],
-        button: 'I am your bb!',
+        answer: ['08-01-23'],
+        buttons: ['I am your bb! 😡'],
         question: `
-            Are you truly my bb?<br>
-            Let\'s prove that by answering some questions.<br>
-            When did we get together? (DD-MM-YY)
+            Umm... really?? Are you truly my bb?<br>
+            Let\'s prove that by answering some questions!<br><br>
+            When was our first kiss? 💋 (DD-MM-YY)
+        `,
+    },
+    {
+        answer: ['3', 'idiots'],
+        buttons: ['Hmmm... He told me but I can\'t remember... 🤔', 'Easy! 💪'],
+        question: `
+            Oh you know that! Malu I... >.< <br>
+            Next question, what is my favourite movie? 🍿
+        `,
+    },
+    {
+        answer: ['singapore'],
+        buttons: ['Let me use my telephatic power... 🧠'],
+        question: `
+            Another one, what is my favourite city? 🤓
+        `,
+    },
+    {
+        answer: ['respect', 'time'],
+        buttons: ['This is tricky...', 'To be sexy...? 🫠'],
+        question: `
+            Let's switch to a deeper question... <br>
+            What is one thing that I am inspired by you? 🥰
         `,
     },
     {
         answer: ['nasi padang', 'bumbu rendang', 'perkedel', 'nangka', 'telur dadar'],
-        question: 'What is my favourite food?',
+        buttons: ['Must be this! 🍱', 'I won\'t give up.'],
+        question: `
+            Awww.... 🥰 <br>
+            Last but not least.... 😈 <br>
+            What is my favourite food?
+        `,
+    },
+    {
+        answer: ['pulang'],
+        buttons: ['Are you sure? 🤔', 'Not daily geli geli time?? 🥺'],
+        question: `
+            Okay okay... I am sure you are my bb! 🤍🤍🤍 <br>
+            Muahhhhhh.... 💋<br><br>
+
+            Happy 5th monthiversarry bb... You have answered 5 questions correctly. <br><br>
+            Just like our tradition, you get a reward. <br>
+            I will grant you one wish. What is your wish? 🥰
+        `,
+    },
+    {
+        buttons: ['OMGGGGGGGGGGGGGGG 🥺😍🤗😭💗'],
+        image: 'ticket.png',
+        title: `
+            TAAAAAAAA-DAAAAAAAAA! 🥳🍾🎉
+        `,
+    },
+    {
+        answer: ['march'],
+        buttons: ['Hmmmm..... 🤔'],
+        question: `
+            Ummm... I <b>might</b> have another gift for you 🥰<br><br>
+            But first you have to answer this correctly.<br>
+            When did I buy the ticket?
+        `,
+    },
+    {
+        answer: ['stay', 'weekend'],
+        buttons: ['Muahhhhh! 💋'],
+        question: `
+            Hehehe.. smart bb! 🥰 Muahhhh! 💋<br>
+            Okay... you may have another wish. What is your wish then?
+        `,
+    },
+    {
+        buttons: ['YAYYY!!! Cuddle and geli2 time all day 🙈'],
+        title: `
+            🥰🥰🥰🥰🥰<br><br>
+            I would love to stay at your place from Saturday Sept 2nd - Tuesday Sept 5th.<br>
+            Then let's arrange a surprise birthday dinner for my mom at the 5th (...or maybe at the 6th 😘).
+        `,
+    },
+    {
+        answer: ['letter'],
+        buttons: ['I uda basah bb... 😋'],
+        question: `
+            Okay... one more thing...<br><br>
+            I actually have a third gift for you.<br>
+            What would that be, my bb? 😘
+        `,
+    },
+    {
+        buttons: ['Read my bb\'s first letter to me! 💌'],
+        title: `
+            Happy 5th monthiversary bb..<br>
+            🤍🤍🤍🤍🤍<br><br>
+            Here's to 5x5x5x5x5x5x5x5 more months together with you. <br>
+            I love you.
+        `,
     },
 ];
 
-const {state, next} = useCycleList(slides);
+const {state, index, next} = useCycleList(slides, {initialValue: slides[0]});
 
 const answer = ref('');
 const error = ref('');
@@ -35,15 +125,22 @@ function generateError(correct: number, total: number) {
     const percent = Math.floor(correct * 100 / total);
 
     const messages = [
-        'Almost, try again!',
-        'Coba tanya Albert lagi.',
-        'Don\'t give up bb!',
+        'Almost... try again! 💪',
+        'Coba tanya Albert lagi deh.. 😉',
+        'Don\'t give up bb! You can do this! 🤗',
+        'Ganbatte.... 加油! (i bukan wibu >.<)',
+        'Ah... u kenal i ga sii... 🧐',
     ];
 
     return `${messages[Math.floor(Math.random() * messages.length)]} (${percent}%)`;
 }
 
 function submit() {
+    if (index.value === slides.length - 1) {
+        window.location.href = '/onion-koala/letter';
+        return;
+    }
+
     if (state.value.question) {
         let correct = 0;
         const correctAnswer = state.value.answer;
@@ -70,11 +167,17 @@ function submit() {
 </script>
 
 <template>
-    <section class="flex-center h-screen flex-col text-center text-gray">
+    <section class="flex-center relative h-screen flex-col text-center text-gray">
+        <div
+            class="absolute right-0 top-0 h-4 w-4"
+            @click="next()"
+        />
+
         <img
             v-if="state.image"
             class="mb-8 rounded"
             :src="`/images/onion-koala/${state.image}`"
+            width="420"
         >
 
         <h1
@@ -100,11 +203,15 @@ function submit() {
             v-html="error"
         />
 
-        <button
-            class="mt-8 rounded border px-4 py-2 hover:bg-gray hover:text-black"
-            @click="submit()"
-        >
-            {{ state.button || 'OK' }}
-        </button>
+        <div class="flex gap-4">
+            <button
+                v-for="(button, i) in state.buttons"
+                :key="i"
+                class="mt-8 rounded border px-4 py-2 hover:bg-gray hover:text-black"
+                @click="submit()"
+            >
+                {{ button }}
+            </button>
+        </div>
     </section>
 </template>
