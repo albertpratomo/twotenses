d3cc8a6ecf0b476c60c597692913ed40dbfdbd5d
Albert Pratomo <albertpratomo21@gmail.com>
2023-07-03 00:49:46 +0200

    feat: add Index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,7 @@ export default defineConfig({
                 Header: 'storyblok/Header',
                 Hero: 'storyblok/Hero',
                 ImageParagraph: 'storyblok/ImageParagraph',
+                Index: 'storyblok/Index',
                 ModelParagraph: 'storyblok/ModelParagraph',
                 OneImage: 'storyblok/OneImage',
                 Page: 'storyblok/Page',
