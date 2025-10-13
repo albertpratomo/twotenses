a7372f5803c0394ad35513f20edca774d4914871
Albert Pratomo <albertpratomo21@gmail.com>
2025-10-13 22:49:19 +0200

    chore: upgrade to tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,56 +2,61 @@ import {defineConfig} from 'astro/config';
 import {loadEnv} from 'vite';
 import sitemap from '@astrojs/sitemap';
 import {storyblok} from '@storyblok/astro';
-import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import vue from '@astrojs/vue';
+import tailwindcss from '@tailwindcss/vite';
 
 const env = loadEnv('', process.cwd(), '');
 const ssr = env.SSR === 'true';
 
 export default defineConfig({
-    adapter: ssr ? vercel() : undefined,
-    integrations: [
-        storyblok({
-            accessToken: env.STORYBLOK_TOKEN,
-            bridge: ssr,
-            components: {
-                Accordion: 'storyblok/Accordion',
-                ArrowLink: 'storyblok/ArrowLink',
-                Carousel: 'storyblok/Carousel',
-                ClientTimeline: 'storyblok/ClientTimeline',
-                Contact: 'storyblok/Contact',
-                Counter: 'storyblok/Counter',
-                FullImage: 'storyblok/FullImage',
-                Header: 'storyblok/Header',
-                Hero: 'storyblok/Hero',
-                ImageParagraph: 'storyblok/ImageParagraph',
-                Index: 'storyblok/Index',
-                LogosGrid: 'storyblok/LogosGrid',
-                ModelParagraph: 'storyblok/ModelParagraph',
-                OneImage: 'storyblok/OneImage',
-                OneParagraph: 'storyblok/OneParagraph',
-                Page: 'storyblok/Page',
-                ParagraphImageGrid: 'storyblok/ParagraphImageGrid',
-                Project: 'storyblok/Project',
-                ProjectInfo: 'storyblok/ProjectInfo',
-                ProjectsGrid: 'storyblok/ProjectsGrid',
-                ServiceList: 'storyblok/ServiceList',
-                TextsGrid: 'storyblok/TextsGrid',
-                TitleParagraph: 'storyblok/TitleParagraph',
-                TwoImage: 'storyblok/TwoImage',
-            },
-        }),
-        sitemap({
-            filter: p => !p.includes('onion-koala'),
-        }),
-        tailwind({config: {applyBaseStyles: false}}),
-        vue({
-            script: {
-                defineModel: true,
-            },
-        }),
-    ],
-    output: ssr ? 'server' : 'static',
-    site: 'https://www.twotenses.com',
+  adapter: ssr ? vercel() : undefined,
+
+  integrations: [
+      storyblok({
+          accessToken: env.STORYBLOK_TOKEN,
+          bridge: ssr,
+          components: {
+              Accordion: 'storyblok/Accordion',
+              ArrowLink: 'storyblok/ArrowLink',
+              Carousel: 'storyblok/Carousel',
+              ClientTimeline: 'storyblok/ClientTimeline',
+              Contact: 'storyblok/Contact',
+              Counter: 'storyblok/Counter',
+              FullImage: 'storyblok/FullImage',
+              Header: 'storyblok/Header',
+              Hero: 'storyblok/Hero',
+              ImageParagraph: 'storyblok/ImageParagraph',
+              Index: 'storyblok/Index',
+              LogosGrid: 'storyblok/LogosGrid',
+              ModelParagraph: 'storyblok/ModelParagraph',
+              OneImage: 'storyblok/OneImage',
+              OneParagraph: 'storyblok/OneParagraph',
+              Page: 'storyblok/Page',
+              ParagraphImageGrid: 'storyblok/ParagraphImageGrid',
+              Project: 'storyblok/Project',
+              ProjectInfo: 'storyblok/ProjectInfo',
+              ProjectsGrid: 'storyblok/ProjectsGrid',
+              ServiceList: 'storyblok/ServiceList',
+              TextsGrid: 'storyblok/TextsGrid',
+              TitleParagraph: 'storyblok/TitleParagraph',
+              TwoImage: 'storyblok/TwoImage',
+          },
+      }),
+      sitemap({
+          filter: p => !p.includes('onion-koala'),
+      }),
+      vue({
+          script: {
+              defineModel: true,
+          },
+      }),
+  ],
+
+  output: ssr ? 'server' : 'static',
+  site: 'https://www.twotenses.com',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
